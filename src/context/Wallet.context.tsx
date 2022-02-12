006cf9b4861921d4094b899c89ccdf8b1fd1e248
Cecilia <cserafini@protonmail.com>
2022-02-12 00:31:23 -0300

    [etb-77538224] Ethereum development enviroment with HardHat configuration

--- src/context/Wallet.context.tsx
+++ src/context/Wallet.context.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { createWalletStore } from './Wallet.store'
-import { useLocalStore } from 'mobx-react'
 
-export const WalletContext = React.createContext<Record<string, any> | null>(null);
 
-export const WalletProvider = ({children} : any) => {
-  const walletStore = useLocalStore(createWalletStore);
-
-  return <WalletContext.Provider value={walletStore}>
-    {children}
-  </WalletContext.Provider>
+export const WalletProvider = () => {
+  return (
+    <></>
+  )
 }
 
-export const useWalletStore = () => React.useContext(WalletContext);
+export default WalletProvider
